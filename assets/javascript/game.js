4036627649c6df0fb302c4291fe2f08dc8d89410
Nadia27 <jones.nadia.l@gmail.com>
2017-12-06 18:44:26 -0500

    Added event key  and index of random word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,10 @@ var guessWords = ['run dmc', 'nas', 'turntable', 'outkast', 'freestyle', 'graffi
 var randomWords; 
 var underScore = []; 
 var userGuess = []; 
+var wins= 0; 
+var loss= 0; 
+var guessesLeft= 10;
+var wrongLetter =[];  
 
 
 
@@ -16,14 +20,42 @@ var userGuess = [];
 function startGame() {
 //Generate randomWords 
 	randomWords = guessWords[Math.floor(Math.random() * guessWords.length)]; 
-	
+		console.log('random word:' + randomWords);
 	for(var i =0; i < randomWords.length; i++) {
 //Generate underScores based on length of randomWords 
 		underScore.push('_'); 
 	} 
-	document.getElementById('blanks').innerHTML= "testing"; 
-//
+	document.getElementById('blanks').innerHTML= underScore.join(" "); 
+
+//variables to reset at start of game
+	wrongLetter = []; 
+	guessesLeft =10; 
+
+	document.getElementById("guessesLeft").innerHTML += guessesLeft;
 }
+
+//Get user input
+
+ userGuess= document.getElementById("user-text");
+
+
+ document.onkeyup = function(event) {
+        
+         userGuess= event.key; 
+         	//Check if letters match random word
+      		if(randomWords.indexOf(userGuess) > -1) {
+      			
+      		 
+      		} else {
+      			wrongLetter.push(userGuess); 
+      			document.getElementById("wrong").innerHTML += wrongLetter; 
+      			
+      		}
+
+
+}
+
+
 
 
 startGame(); 
