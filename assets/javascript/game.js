e38653096516f5aad615fa56f5a002fc89cf3041
Nadia27 <jones.nadia.l@gmail.com>
2017-12-06 16:15:35 -0500

    new js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,29 @@
-//User Input
+//Variables
 
-var playerName= prompt("Yo, let's play Hip-Hop Hangman. Enter your name: "); 
-	
-
-function gamer() {
-	document.getElementById("user").innerHTML = 'Welcome :' + playerName; 
-}
-
-
- //Arrays
-//*Choose words randomly
 var guessWords = ['run dmc', 'nas', 'turntable', 'outkast', 'freestyle', 'graffiti', 
 				'poetry', 'the breaks', 'no limit', 'j cole', 'nwa', "mic check", 'jay z', 
 				'rick rubin', 'queen latifah', 'disc jockey', 'beatbox', 'gold chains', 'adidas', 'mixtape', 'the source' ];
 
-var randomWord = guessWords[Math.floor(Math.random() * guessWords.length)];
-let choosenWord = guessWords[randomWord];
-console.log(randomWord); 
+var randomWords; 
+var underScore = []; 
+var userGuess = []; 
 
 
-//create underscore based on length of word
 
-/*var underScore = []; 
 
-var createUnderscore = () => {
+//Function
 
-	for(var i = 0; i < choosenWord.length; i ++) {
-		underScore.push('_'); 
-	}
+function startGame() {
+//Generate randomWords 
+	randomWords = guessWords[Math.floor(Math.random() * guessWords.length)]; 
 	
-} 
-
-console.log(createUnderscore()); */
-
-
-
-
-//get player input
-
-var userGuess= document.getElementById("user-text");
+	for(var i =0; i < randomWords.length; i++) {
+//Generate underScores based on length of randomWords 
+		underScore.push('_'); 
+	} 
+	document.getElementById('blanks').innerHTML= "testing"; 
+//
+}
 
 
- document.onkeyup = function(event) {
-         userGuess.textContent = event.key;
-        console.log(event); 
-      }; 
-//check if guess is right
-//if right put in right array
-//if wrong push to gu
+startGame(); 
