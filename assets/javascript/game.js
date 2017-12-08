1f67f2897e5437266a9fdf7988d6707c9ba9dc41
Nadia27 <jones.nadia.l@gmail.com>
2017-12-07 23:33:12 -0500

    Added counter for guesses left/underscore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,10 @@ var guessWords = ['run dmc', 'nas', 'turntable', 'outkast', 'freestyle', 'graffi
 var randomWords; 
 var underScore = []; 
 var userGuess = []; 
-var wins= 0; 
-var loss= 0; 
+var wins= 0;  
 var guessesLeft= 10;
-var wrongLetter =[];  
+var wrongLetter =[]; 
+var rightLetter = [];  
 
 
 
@@ -20,7 +20,7 @@ var wrongLetter =[];
 function startGame() {
 //Generate randomWords 
 	randomWords = guessWords[Math.floor(Math.random() * guessWords.length)]; 
-		console.log('random word:' + randomWords);
+		console.log('random word:' +randomWords);
 	for(var i =0; i < randomWords.length; i++) {
 //Generate underScores based on length of randomWords 
 		underScore.push('_'); 
@@ -36,7 +36,11 @@ function startGame() {
 
 //Get user input
 
- userGuess= document.getElementById("user-text");
+function winLose() {
+	if(guessesLeft.indexOf=0);
+
+	alert("You lose!");
+}
 
 
  document.onkeyup = function(event) {
@@ -45,11 +49,25 @@ function startGame() {
          	//Check if letters match random word
       		if(randomWords.indexOf(userGuess) > -1) {
       			
-      		 
-      		} else {
-      			wrongLetter.push(userGuess); 
-      			document.getElementById("wrong").innerHTML += wrongLetter; 
+      			for(var i =0; i < randomWords.length; i ++) {
+
+      				if(randomWords[i] === userGuess){
+      					
+      					underScore[i] =userGuess; 
+      					console.log(underScore);
+					}
+      			}
       			
+      		 
+      		}else{
+
+      			wrongLetter.push(userGuess); 
+      			document.getElementById("wrong").innerHTML = wrongLetter; 
+      			
+                  console.log(wrongLeft); 
+
+            guessesLeft --; 
+      			console.log(guessesLeft); 
       		}
 
 
