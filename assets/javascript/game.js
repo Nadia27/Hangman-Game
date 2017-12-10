be9873ea33019201db477d7988297ec897e036bb
Nadia27 <jones.nadia.l@gmail.com>
2017-12-09 20:11:17 -0500

    fixed underscores and guesses left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,15 @@ var guessWords = ['run dmc', 'nas', 'turntable', 'outkast', 'freestyle', 'graffi
 				'poetry', 'the breaks', 'no limit', 'j cole', 'nwa', "mic check", 'jay z', 
 				'rick rubin', 'queen latifah', 'disc jockey', 'beatbox', 'gold chains', 'adidas', 'mixtape', 'the source' ];
 
-var randomWords; 
+var randomWords =""; 
 var underScore = []; 
 var userGuess = []; 
-var wins= 0;  
+var wins= 0; 
+var namelength = 0; 
 var guessesLeft= 10;
 var wrongLetter =[]; 
 var rightLetter = [];  
-
+var chosenArtist = "";
 
 
 
@@ -21,32 +22,67 @@ function startGame() {
 //Generate randomWords 
 	randomWords = guessWords[Math.floor(Math.random() * guessWords.length)]; 
 		console.log('random word:' +randomWords);
-	for(var i =0; i < randomWords.length; i++) {
-//Generate underScores based on length of randomWords 
-		underScore.push('_'); 
+      chosenArtist = randomWords.split("");
+      namelength = chosenArtist.length;
+
+//Generate underScores based on length of randomWords 	
+   for(var i =0; i < namelength; i++) {
+
+		underScore.push("_"); 
 	} 
-	document.getElementById('blanks').innerHTML= underScore.join(" "); 
+	document.getElementById("blanks").innerHTML= underScore.join(" "); 
 
 //variables to reset at start of game
 	wrongLetter = []; 
 	guessesLeft =10; 
 
-	document.getElementById("guessesLeft").innerHTML += guessesLeft;
+	document.getElementById("guessCounter").innerHTML += guessesLeft;
+   
 }
 
 //Get user input
-
+//you need to figure this section out!!
+//alert win/lose
 function winLose() {
-	if(guessesLeft.indexOf=0);
 
-	alert("You lose!");
-}
+   if (userGuess == randomWords) {
 
+      console.log(winLose); 
+   
+   }else {
+
+      alert("You lose: " + "the word was" + randomWords); 
+
+
+
+
+   }
+
+      
+	
+} 
+keyguess = false;
 
  document.onkeyup = function(event) {
         
-         userGuess= event.key; 
-         	//Check if letters match random word
+         //userGuess= event.key; 
+       var userGuess =String.fromCharCode(event.keyCode).toLowerCase();
+
+             for (var i = 0; i < underScore.length; i++) {
+               
+               if (randomWords[i] == userGuess){
+                  keyguess = true;
+                  underScore[i] = userGuess;
+                 
+                  document.getElementById("blanks").innerHTML =  underScore.join(" ");
+                 // console.log(correct);
+
+             }
+            
+
+         };
+
+         	//Check if userGuess match randomWord
       		if(randomWords.indexOf(userGuess) > -1) {
       			
       			for(var i =0; i < randomWords.length; i ++) {
@@ -55,19 +91,20 @@ function winLose() {
       					
       					underScore[i] =userGuess; 
       					console.log(underScore);
-					}
+					    }
       			}
       			
       		 
-      		}else{
+      		}else{// this needs to be figured out too!!
 
       			wrongLetter.push(userGuess); 
-      			document.getElementById("wrong").innerHTML = wrongLetter; 
+      			document.getElementById("wrong").innerHTML += wrongLetter; 
       			
-                  console.log(wrongLeft); 
-
-            guessesLeft --; 
-      			console.log(guessesLeft); 
+                  
+               //decrementing but cannot get to print to screen
+               guessesLeft--; 
+      			document.getElementById("guessCounter").innerHTML = guessesLeft;
+                  console.log(guessesLeft);
       		}
 
 
